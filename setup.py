c885f3f0cc310b6faa99f3a4f2fb2d0a0c850141
siltoon <siltoon1@gmail.com>
2019-02-28 13:21:07 +0100

    Last commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='EZmodel',
+      version='0.1',
+      description='Easy model construction module (with Keras)',
+      url='http://github.com/dl4d/ezmodel',
+      author='Fab.Da',
+      author_email='deeplearningformation@gmail.com',
+      license='MIT',
+      packages=['ezmodel'],
+      zip_safe=False)
